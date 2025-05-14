134af7bbecd5ee03f6f4c95b2a83b3ca8ee42efb
Md. Mostofa Hasib <90277109+Hasib2202@users.noreply.github.com>
2025-05-14 20:08:48 +0600

    role based access account page

--- ProjectMiniAccountManagementSystem/ProjectMiniAccountManagementSystem/Pages/Accounts/ChartOfAccounts.cshtml.cs
+++ ProjectMiniAccountManagementSystem/ProjectMiniAccountManagementSystem/Pages/Accounts/ChartOfAccounts.cshtml.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ProjectMiniAccountManagementSystem.Pages.Accounts
 {
-//role based access
+//role based access.
     [Authorize(Roles = "Admin,Accountant,Viewer")]
     public class ChartOfAccountsModel : PageModel
     {
